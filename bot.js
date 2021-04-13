fe5cf05f26ace788a7ed5d2f263ca670a222c7b2
CptPalindrome <bpendleton2018@gmail.com>
2021-04-13 00:21:35 -0500

    removed h.h, it didn't work on the server version but did on local because of course not

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client, Attachment } = require('discord.js');
+const { Client } = require('discord.js');
 const auth = require('./auth.json');
 const winston = require('winston');
 const moment = require('moment');
@@ -50,11 +50,6 @@ client.on('message', msg => {
                 
                 case 'git':
                     msg.channel.send(`README & Source Code here: https://github.com/CptPalindrome/HonBot`);
-                    break;
-
-                case 'h':
-                    const attachment = new Attachment('./media/h.gif');
-                    msg.channel.send(attachment);
                     break;
 
                 case 'help':
