8a231042e4364fb9f570b5dd75154415b776013f
CptPalindrome <bpendleton2018@gmail.com>
2022-03-08 10:38:54 -0600

    Rounding conversion output so it isn't hideous

--- temperatureConversion/converter.js
+++ temperatureConversion/converter.js
@@ -1,9 +1,9 @@
 function c2f(temp) {
-    return (temp *  (9/5)) + 32;
+    return Math.round((temp *  (9/5)) + 32);
 }
 
 function f2c(temp) {
-    return (temp - 32) * (5/9);
+    return Math.round((temp - 32) * (5/9));
 }
 
 module.exports = { c2f, f2c };
